e652804b0e58e19714f61963ae06f63dc58c8f10
arkag30 <arkag30@gmail.com>
2019-09-30 19:44:34 +0530

    voice output

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const greetings = [
     'please keep quite'
 ];
 const weather = ['How does it matter you are not even going to go out you lazy Bastard'];
-
+const hi=['hello you little goofball'];
+const propose=['get a room','you are soooo lonely','i hate you'];
 const SpeechRecognition=window.SpeechRecognition||window.webkitSpeechRecognition;
 const recognition = new SpeechRecognition();
 
@@ -17,6 +18,7 @@ recognition.onresult= function(event){
     const current = event.resultIndex;
     const transcript=event.results[current][0].transcript;
     content.textContent=transcript;
+    readOutLoud(transcript);
 };
 
 
@@ -28,10 +30,22 @@ btn.addEventListener('click',()=>{
 function readOutLoud(message){
     const speech = new SpeechSynthesisUtterance(message);
      speech.text="Shut up and Stop Bugging me";
-      if(message.includes('how are you')){
+      if(message.includes('how are you'||message.includes('whats up'))){
          const finalText= greetings[Math.floor(Math.random()*greetings.length)];
       speech.text=finalText;
      }
+     if(message.includes('weather')){
+        const finalText= weather;
+     speech.text=finalText;
+    }
+    if(message.includes('hello')||message.includes('hi')){
+        const finalText= hi;
+     speech.text=finalText;
+    }
+    if(message.includes('i love you')){
+        const finalText= propose[Math.floor(Math.random()*propose.length)];
+     speech.text=finalText;
+    }
     speech.volume= 1;
     speech.rate= 1;
     speech.pitch= 1;
